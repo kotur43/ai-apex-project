b754ca9de5329684a4e9c907d0b2a3d51e99d9ba
Dusan Koturovic <dusan@mx-laptop>
2025-11-26 23:21:27 +0100

    Updated main.py file

--- app/main.py
+++ app/main.py
@@ -5,3 +5,11 @@ app = FastAPI(title="AI + APEX Project")
 @app.get("/")
 def read_root():
     return {"message": "Hello, AI + APEX World!"}
+
+# -------------------------
+# GET with path parameters
+# Example: /hello/Dusan
+# -------------------------
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"message": f"Hello, {name}!"}
